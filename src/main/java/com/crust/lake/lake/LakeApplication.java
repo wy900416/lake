7bb985695a85ec254d81f6ecf114fac8a2a651a1
Tony <yukun0416@gmail.com>
2021-04-20 08:36:42 +0800

    Initial commit

---
+++ src/main/java/com/crust/lake/lake/LakeApplication.java
@@ -0,0 +1,13 @@
+package com.crust.lake.lake;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LakeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LakeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/crust/lake/lake/LakeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crust.lake.lake;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LakeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
